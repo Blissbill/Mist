b1368ff0f27300399362f74fb94bee1f0f0ba6d3
Blissbill <ne.rom4nov@gmail.com>
2022-11-10 12:54:25 +0500

    feat: added glm

--- premake5.lua
+++ premake5.lua
@@ -16,6 +16,8 @@ IncludeDir = {}
 IncludeDir["GLFW"] = "Mist/vendor/GLFW/include"
 IncludeDir["Glad"] = "Mist/vendor/Glad/include"
 IncludeDir["ImGui"] = "Mist/vendor/imgui"
+IncludeDir["glm"] = "Mist/vendor/glm"
+
 group "Dependencies"
 	include "Mist/vendor/GLFW"
 	include "Mist/vendor/Glad"
@@ -39,6 +41,8 @@ project "Mist"
 	{
 		"%{prj.name}/src/**.h",
 		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/vendor/glm/glm/**.hpp",
+		"%{prj.name}/vendor/glm/glm/**.inl",
 	}
 
 	includedirs 
@@ -47,7 +51,8 @@ project "Mist"
 		"%{prj.name}/vendor/spdlog/include",
 		"%{IncludeDir.GLFW}",
 		"%{IncludeDir.Glad}",
-		"%{IncludeDir.ImGui}"
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.glm}"
 	}
 
 	links
@@ -109,7 +114,8 @@ project "Sandbox"
 	includedirs 
 	{
 		"Mist/vendor/spdlog/include",
-		"Mist/src"
+		"Mist/src",
+		"%{IncludeDir.glm}"
 	}
 
 	links
